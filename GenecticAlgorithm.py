554b5eccea3ebe2e864eab74e274cf7f7e8c59b3
Guilherme Eras <135174494+GuilhermeRodriguesEras@users.noreply.github.com>
2026-03-30 13:58:51 +0000

    tratamento inicial da imagem

---
+++ GenecticAlgorithm.py
@@ -0,0 +1,21 @@
+import numpy as np
+from PIL import Image
+from CONFIG import *
+
+#load image
+pil_image = Image.open("img.png").convert('RGB')
+
+#convertSize
+original_size = (pil_image.width, pil_image.height)
+resized_img = pil_image.resize(SIZEIMAGE, Image.Resampling.LANCZOS)
+
+#numpy convert
+image_np = np.asarray(resized_img)
+print(image_np.shape)
+
+#reshape to original data
+pilImageRecreated = Image.fromarray(image_np, 'RGB')
+pilImageRecreated = pilImageRecreated.resize(original_size, Image.Resampling.LANCZOS)
+
+#save final result
+pilImageRecreated.save("output.png")
